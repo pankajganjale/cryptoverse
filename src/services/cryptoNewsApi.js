cec6c8043a2eb84a0351acc2b2e6237a7bad229f
Pankaj Ganjale <pankajganjale2805@gmail.com>
2021-12-05 10:47:34 +0530

    Minor Change

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -32,14 +32,16 @@ const cryptoNewsHeaders = {
   'x-rapidapi-host': "a8924a5502mshb3bb86466be27e4p19e6c6jsnccd6b54c4ea0",
 };
 
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
 export const cryptoNewsApi = createApi({
   reducerPath: 'cryptoNewsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: "https://bing-news-search1.p.rapidapi.com" }),
+  baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptoNews: builder.query({
-      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&limit=${count}`),
     }),
   }),
 });
