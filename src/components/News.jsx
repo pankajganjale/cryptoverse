02abe74ed0ee88547c50a1ec2e8286e5b15e38e4
Pankaj Ganjale <pankajganjale2805@gmail.com>
2022-01-19 18:22:59 +0530

    Minor Change

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -5,7 +5,6 @@ import moment from 'moment';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 import Loader from './Loader';
-// import Loader from './Loader';
 
 const demoImage = 'http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg';
 
